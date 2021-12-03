c06a545d299b9bbb80e043f5437f4b5466cda013
Carlos Castilla <carloscastillaorozco@gmail.com>
2021-12-03 10:07:04 -0500

    Fixed little bug with Anuncios
    Cannot read lenght of array without checking if it exists first

--- src/Initiative/Change.js
+++ src/Initiative/Change.js
@@ -15,17 +15,18 @@ function Change({ user, id }) {
  return (
 
   <div className="initiative">
-   {updates.length > 0 ?
-    <div className="initiative-container">{
-     updates.map(i => {
-      return <div className="container update">
-       <img src={i.img} alt="" />
-       <h3>{i.title}</h3>{i.update}</div>
-     })
-    }
-    </div>
-    : "Aún no hay anuncios para esta iniciativa."
-   }
+   {updates ? {
+    updates.length > 0 ?
+     <div className="initiative-container">{
+      updates.map(i => {
+       return <div className="container update">
+        <img src={i.img} alt="" />
+        <h3>{i.title}</h3>{i.update}</div>
+      })
+     }
+     </div>
+     : "Aún no hay anuncios para esta iniciativa."
+   } : ""}
   </div>
  )
 }
